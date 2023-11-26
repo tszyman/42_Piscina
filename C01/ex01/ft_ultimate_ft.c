68eddbaf46a605e6d09563fbb2e02f41c3f5ce00
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-11-26 21:44:27 +0100

    C01/ex01 done, check by Norm to be sure

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomek <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/26 21:23:45 by tomek             #+#    #+#             */
+/*   Updated: 2023/11/26 21:43:17 by tomek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_ultimat_ft(int *********nbr);
+
+void	ft_ultimate_ft(int *********nbr)
+	{
+	*********nbr = 42;
+}
+
+//int	main(void)
+//{
+//	int	*nbr8;
+//	int	**nbr7;
+//	int	***nbr6;
+//	int	****nbr5;
+//	int	*****nbr4;
+//	int	******nbr3;
+//	int	*******nbr2;
+//	int	********nbr1;
+//	int	*********nbr;
+//	int	num;
+//
+//	num = 21;
+//	printf("Before: %d", num);
+//	printf("\n");
+//	nbr8 = &num;
+//	nbr7 = &nbr8;
+//	nbr6 = &nbr7;
+//	nbr5 = &nbr6;
+//	nbr4 = &nbr5;
+//	nbr3 = &nbr4;
+//	nbr2 = &nbr3;
+//	nbr1 = &nbr2;
+//	nbr = &nbr1;
+//
+//	
+//
+//	ft_ultimate_ft(nbr);
+//	printf("After: %d", num);
+//	return 0;
+//}
