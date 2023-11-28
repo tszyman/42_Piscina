011f9c2423088f87ebb06e86690f08c042adb4b5
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-11-28 23:21:05 +0100

    ex00 corrected, changed to array style

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -6,35 +6,18 @@
 /*   By: tszymans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 18:08:20 by tszymans          #+#    #+#             */
-/*   Updated: 2023/11/28 18:36:58 by tszymans         ###   ########.fr       */
+/*   Updated: 2023/11/28 23:03:03 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2);
 
-int	calculate(char *str)
-{
-	int	i;
-	int	val;
-
-	i = 0;
-	val = 0;
-	while (str[i] != '\0')
-	{
-		val += str[i];
-		i++;
-	}
-	return (val);
-}
-
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	v1;
-	int	v2;
+	unsigned int	i;
 
-	v1 = 0;
-	v2 = 0;
-	v1 = calculate(s1);
-	v2 = calculate(s2);
-	return (v1 - v2);
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+		i++;
+	return (s1[i] - s2[i]);
 }
