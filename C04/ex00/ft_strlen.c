434b04571ec9122d4def50ed861d5191861689e0
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-11-30 09:13:23 +0100

    ex00 done

---
+++ C04/ex00/ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomek <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 08:55:57 by tomek             #+#    #+#             */
+/*   Updated: 2023/11/30 09:09:40 by tomek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_strlen(char *str);
+
+int ft_strlen(char *str)
+{
+	unsigned int	len;
+
+	len = 0;
+	while (*str !='\0')
+		{
+			str++;
+			len++;
+		}
+	return (len);
+}
+
+//int main()
+//{
+//	printf("dlugosc Tomek: %d", ft_strlen("Tomasz"));
+//}
