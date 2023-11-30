3e719a743a0794eadf1de803b766f1a4a635e40b
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-11-30 12:48:16 +0100

    ex00 Norm corrections

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -6,23 +6,23 @@
 /*   By: tomek <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 08:55:57 by tomek             #+#    #+#             */
-/*   Updated: 2023/11/30 09:09:40 by tomek            ###   ########.fr       */
+/*   Updated: 2023/11/30 12:47:11 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 int	ft_strlen(char *str);
 
-int ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
 	unsigned int	len;
 
 	len = 0;
-	while (*str !='\0')
-		{
-			str++;
-			len++;
-		}
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
 	return (len);
 }
 
