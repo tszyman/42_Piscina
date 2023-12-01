2df817e21f8154abc2998bf19b0d7fcac3074bc8
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-12-01 11:37:51 +0100

    All exercises from C04

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomek <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: tszymans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/30 15:16:33 by tomek             #+#    #+#             */
-/*   Updated: 2023/11/30 16:15:19 by tomek            ###   ########.fr       */
+/*   Created: 2023/11/30 18:43:51 by tszymans          #+#    #+#             */
+/*   Updated: 2023/12/01 00:04:41 by tszymans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include <stdio.h>
 
 int	ft_atoi(char *str);
 
@@ -18,32 +18,31 @@ int	ft_atoi(char *str)
 {
 	int				out;
 	unsigned int	minus;
+	unsigned int	i;
 
 	out = 0;
 	minus = 1;
-	while (*str != '\0')
+	i = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
+		i++;
+	while (*str && (str[i] == '+' || str[i] == '-'))
 	{
-		while (*str == ' ' || *str == '\f' || *str == '\n'
-			|| *str == '\r' || *str == '\t' || *str == '\v')
-			str++;
-		while (*str && (*str == '+' || *str == '-'))
-		{
-			if (*str == '-')
-				minus *= -1;
-			str++;
-		}
-		while (*str >= '0' && *str <= '9')
-		{
-			out = (out * 10) + (*str - 48);
-			str++;
-		}
+		if (str[i] == '-')
+			minus *= -1;
+		i++;
 	}
-return (minus * out);
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		out = (out * 10) + (str[i] - 48);
+		i++;
+	}
+	return (minus * out);
 }
 
-int	main(void)
-{
-	char	str[] = "---+--+1234ab567";
-	
-	printf("%d", ft_atoi(str));
-}
+//int	main(void)
+//{
+//	char	str[] = "---+--+1234ab567";
+//	
+//	printf("%d", ft_atoi(str));
+//}
