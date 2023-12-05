6439e773582fdf787c681f33fadff23509afa6aa
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-12-05 10:09:07 +0100

    C05/ex05 done

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -6,16 +6,12 @@
 /*   By: tszymans <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 19:55:01 by tszymans          #+#    #+#             */
-/*   Updated: 2023/12/04 21:14:58 by tszymans         ###   ########.fr       */
+/*   Updated: 2023/12/05 09:58:45 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb);
 
-int ft_rec_pwr(int nb, int pwr)
-{
-	return(nb * ft_rec_pwr(nb, pwr-1));
-}
 int ft_sqrt(int nb)
 {
 	int	i;
@@ -24,18 +20,17 @@ int ft_sqrt(int nb)
 	if (nb < 0)
 		return (0);
 	if (nb == 0)
-		return (1);
+		return (0);
 	if (nb > 0)
 	while (i >= 1)
 	{
-	//	if (ft_rec_pwr(i, 2) == nb)
 		if (i*i == nb)
 			return (i);
 		i++;
 	}
 	return (0);
 }
-#include <stdio.h>
+/*#include <stdio.h>
 int	main()
 {
 	printf("sqrt(%d) = %d (=0)\n", -1, ft_sqrt(-1));
@@ -44,4 +39,4 @@ int	main()
 	printf("sqrt(%d) = %d (=2)\n", 4, ft_sqrt(4));
 	printf("sqrt(%d) = %d (=3)\n", 9, ft_sqrt(9));
 	printf("sqrt(%d) = %d (=0)\n", 6, ft_sqrt(6));
-}
+}*/
