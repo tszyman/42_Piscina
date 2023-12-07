1e1ef51baf678f3b52d010cb37c892f244e91f36
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-12-07 13:46:00 +0100

    Corrected sorting

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -6,13 +6,13 @@
 /*   By: tomek <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 11:03:23 by tomek             #+#    #+#             */
-/*   Updated: 2023/12/07 13:17:37 by tomek            ###   ########.fr       */
+/*   Updated: 2023/12/07 13:36:02 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_rev_int_tab(int *tab, int size);
+void	ft_sort_int_tab(int *tab, int size);
 
-void	ft_rev_int_tab(int *tab, int size)
+void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
 	int	j;
@@ -42,7 +42,7 @@ int	main()
 	int	tab[] = {8, 4, 3, 7, 5, 2};
 	int size = 6;
 
-	ft_rev_int_tab(tab, size);
+	ft_sort_int_tab(tab, size);
 	printf("%d, %d, %d, %d, %d, %d", tab[0], /n 
 	tab[1], tab[2], tab[3], tab[4], tab[5]);
 	return (0);
