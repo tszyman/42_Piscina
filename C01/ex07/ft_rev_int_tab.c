9c0d42e059087ca206dd65d212ea626b8c776b84
Tomasz Szymanski <tomasz_szymanski@outlook.com>
2023-12-07 13:45:22 +0100

    Corrected reverse

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: tomek <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 11:03:23 by tomek             #+#    #+#             */
-/*   Updated: 2023/12/07 11:50:15 by tomek            ###   ########.fr       */
+/*   Updated: 2023/12/07 13:43:40 by tomek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@ void	ft_rev_int_tab(int *tab, int size)
 	int	buf;
 
 	i = 0;
-	while (i <= size/2)
+	while (i < size/2)
 	{
-		buf = tab[i];
-		tab[i] = tab[size - 1 - i];
-		tab[size - 1 - i] = buf;
+		buf = tab[size - 1 - i];
+		tab[size - 1 - i] = tab[i];
+		tab[i] = buf;
 		i++;
 	}
 }
@@ -30,10 +30,11 @@ void	ft_rev_int_tab(int *tab, int size)
 #include <stdio.h>
 int	main()
 {
-	int	tab[] = {8, 4, 3, 7, 5, 2};
-	int size = 6;
+	int	tab[] = {8, 4, 3, 5, 7, 5, 2};
+	int size = 7;
 
+	printf("%d, %d, %d, %d, %d, %d, %d\n", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], tab[6]);
 	ft_rev_int_tab(tab, size);
-	printf("%d, %d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5]);
+	printf("%d, %d, %d, %d, %d, %d, %d", tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], tab[6]);
 	return (0);
 }*/
